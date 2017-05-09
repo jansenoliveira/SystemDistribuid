1be46dcef47929887e601fcc39e2ad4ef8754637
Mauro de Carvalho <mauro.oak@outlook.com>
2017-05-08 23:29:33 -0300

    Mudanças de enconding

--- RPC/server.py
+++ RPC/server.py
@@ -3,7 +3,7 @@ from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
 
 #https://docs.python.org/2/library/simplexmlrpcserver.html
 
-# Restringir a um caminho específico
+# Restringir a um caminho especifico
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
 
@@ -11,23 +11,23 @@ class RequestHandler(SimpleXMLRPCRequestHandler):
 server = SimpleXMLRPCServer(("localhost", 8000),
                             requestHandler=RequestHandler)
 
-#Registrar as funções de introspecção XML-RPC system.listMethods, system.methodHelp e system.methodSignature
+#Registrar as funcoes de introspeccao XML-RPC system.listMethods, system.methodHelp e system.methodSignature
 server.register_introspection_functions()
 
 
-# Registro de pow() função; Isso usará o valor de
-# pow .__ name__ como o nome, que é apenas 'pow'.
+# Registro de pow() funcao; Isso usara o valor de
+# pow .__ name__ como o nome, que e apenas 'pow'.
 server.register_function(pow)
 
 
-# Registrar uma função com um nome diferente
+# Registrar uma funcao com um nome diferente
 def adder_function(x,y):
     return x + y
 server.register_function(adder_function, 'add')
 
 
-# Registrar uma instância; Todos os métodos da instância são
-# publicado como XML-RPC métodos (neste caso, apenas 'div').
+# Registrar uma instancia; Todos os metodos da instancia sao
+# publicado como XML-RPC metodos (neste caso, apenas 'div').
 class MyFuncs:
     def div(self, x, y):
         return x // y
