2dcc1a90d9786f37da8b427def460e5e409cafb8
Mauro de Carvalho <mauro.oak@outlook.com>
2017-06-04 22:43:18 -0300

    Criando um consumidor com o Jquery

---
+++ REST/README.md
@@ -0,0 +1,11 @@
+# REST
+
+Exemplo de implementação de um RESTful API usando Flask
+
+### Resolvendo Problema do CORS
+- [https://flask-cors.readthedocs.io/en/latest/](https://flask-cors.readthedocs.io/en/latest/)
+
+
+## Links:
+- [https://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask](https://blog.miguelgrinberg.com/post/designing-a-restful-api-with-python-and-flask)
+
--- REST/app.py
+++ REST/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify,make_response
+from flask_cors import CORS, cross_origin # Problema
 
 app = Flask(__name__)
+CORS(app)
 
 tasks = [
 	{
@@ -20,11 +22,11 @@ tasks = [
 
 @app.route('/')
 def index():
-	return "Sanity Test"
+	return "URL PARA O ACESSO DA API: '/api/tasks'"
 
 @app.route('/api/tasks', methods=['GET'])
 def get_tasks():
-	return jsonify({'tasks': tasks})
+	return make_response(jsonify({'tasks': tasks}), 200)
 
 if __name__ == '__main__':
 	app.run(debug=True)
